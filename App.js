1787bab3e9482e06901cb3e636e959cc7d6e2cff
TaeKyong Kim <taekyongkim@TaeKyongui-iMac.local>
2018-12-25 21:59:24 +0900

    tapNavigator edit

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import {
+  Button,
   Text,
   View
 } from 'react-native';
 import {
+  Ionicons
+} from '@expo/vector-icons';
+import {
+  createStackNavigator,
   createBottomTabNavigator,
   createAppContainer
 } from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class HomeScreen extends React.Component {
   render() {
     return ( 
-    <View style = {
+      <View style = {
         {
           flex: 1,
           justifyContent: 'center',
           alignItems: 'center'
         }
-      }>
+      } >
       <Text > Home! </Text> 
+      <Button title = "Go to Settings" onPress = { () => this.props.navigation.navigate('Settings')}/> 
+      <Button title = "Go to Details" onPress = { () => this.props.navigation.navigate('Details')}/> 
       </View>
     );
   }
@@ -28,27 +34,67 @@ class HomeScreen extends React.Component {
 class SettingsScreen extends React.Component {
   render() {
     return ( 
-      <View style = {{
+      <View style = {
+        {
           flex: 1,
           justifyContent: 'center',
           alignItems: 'center'
-        }}>
-      <Text> Settings! </Text> 
+        }
+      }>
+      <Text > Settings! </Text> 
+      <Button title = "Go to Home" onPress = {() => this.props.navigation.navigate('Home')}/> 
+      <Button title = "Go to Details" onPress = {() => this.props.navigation.navigate('Details')}/> 
       </View>
     );
   }
 }
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
+class DetailsScreen extends React.Component {
+  render() {
+    return ( 
+      <View style = {{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }
+      }>
+      <Text> Details! </Text> 
+      </View>
+    );
+  }
+}
+
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Details: {
+    screen: DetailsScreen
+  },
+});
+
+const SettingsStack = createStackNavigator({
+  Settings: {
+    screen: SettingsScreen
+  },
+  Details: {
+    screen: DetailsScreen
+  },
+});
+
+export default createAppContainer(createBottomTabNavigator({
+  Home: {
+    screen: HomeStack
+  },
+  Settings: {
+    screen: SettingsStack
+  },
 }, {
   defaultNavigationOptions: ({
     navigation
   }) => ({
     tabBarIcon: ({
       focused,
-      horizontal,
       tintColor
     }) => {
       const {
@@ -67,7 +113,7 @@ const TabNavigator = createBottomTabNavigator({
         iconName
       }
       size = {
-        horizontal ? 20 : 25
+        25
       }
       color = {
         tintColor
@@ -79,6 +125,4 @@ const TabNavigator = createBottomTabNavigator({
     activeTintColor: 'tomato',
     inactiveTintColor: 'gray',
   },
-});
-
-export default createAppContainer(TabNavigator);
+}));
